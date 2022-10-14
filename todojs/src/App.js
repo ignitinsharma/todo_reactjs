a8c612c3acd6b1d4aa5d7e113f33d693380d625b
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-10-14 22:44:06 +0530

    update

--- todojs/src/App.js
+++ todojs/src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import { Todo } from './components/todo';
 import {AddTodo} from "./components/addtodo"
 
 function App() {
   return (
     <div className="App">
-      {/* <Todo/> */}
-      {/* <Maintodo/> */}
+    
     <AddTodo/>
     </div>
   );
--- todojs/src/components/addtodo.jsx
+++ todojs/src/components/addtodo.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
 function AddTodo() {
-
   /* 
   -->  Here firstly take data from user by using setText function and then
         setText function get data and add into Text variable...
   */
   const [text, setText] = useState("");
 
-  
   /* 
 --> We need to manage or store bunch of todos thats why we create this todos state with
     blank array (here todos is array) because if we wanna store multiple value we use array or objects so here
@@ -16,35 +14,31 @@ function AddTodo() {
 */
   const [todos, setTodos] = useState([]);
 
-
-
   let handleaddTodo = () => {
-
     /* 
     --> when user click on add todo button then this function will evoke and then this function 
         store that filled data into object 
     */
 
-      let newitem={
-        title:text,
-        id:Math.random()+text,
-        status:false
-      }
+    let newitem = {
+      title: text,
+      id: Math.random() + Date.now() + text,
+      status: false,
+    };
 
-
-      /* 
+    /* 
       --> Here we are simply this uper object into array which is --> todos and then update into state
           why update state..? because we wanna show array of object that why
       */
 
-      let mainarr=[...todos,newitem]
-
-      /* 
+    let mainarr = [...todos, newitem];
+    //console.log(mainarr);
+    /* 
       --> After adding newitem object into array we simply pass that updated array into 
           setTodos state for showing into DOM..
       */
-     
-      setTodos(mainarr);
+
+    setTodos(mainarr);
   };
 
   return (
@@ -72,11 +66,25 @@ function AddTodo() {
         --> Onclick on button we need to add todo and update and thats why weadd that 
         function --> handleaddTodo function when user click so that function will envoke...
         */}
-        <button className="headerbtn">Submit Task</button>
+        <button onClick={handleaddTodo} className="headerbtn">
+          Submit Task
+        </button>
       </div>
 
-      <div>
-        <h1>{text}</h1>
+      <div className="taskrendor">
+        <div className="rendortodos">
+          {/* 
+          --> This map function do nothing but print the arrays elements and return it
+              we apply on that todos array that map function...
+          */}
+          {todos.map((el) => ( // el is nothing but parameter of arrow function
+            <p className="h3todo">{el.title}
+            <div className="togglediv">
+              <p>{el.status?"Done 😍":"Pending 👀"}</p>
+            </div>
+            </p>
+          ))}
+        </div>
       </div>
     </>
   );
